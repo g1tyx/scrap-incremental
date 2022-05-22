8e6bccc3144dc5f664b5117634adf6a720dc2100
Melk12345 <105999204+Melk12345@users.noreply.github.com>
2022-05-21 18:37:56 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,255 @@
+// let scrap = 11;
+// let totalScrap = 11;
+// let scrapsThisRun = 0;
+// let generators = [];
+// let cost = [];
+// let transistors = 0;
+// let transistorsBonus = 1.02;
+
+let data = {
+    scrap: 11,
+    totalScrap: 11,
+    scrapsThisRun: 0,
+    generators: [],
+    cost: [],
+    transistors: 0,
+    transistorsBonus: 1.02,
+}
+
+for (let i = 0; i < 8; i++) {
+    let generator = {
+        amount: 0,
+        sps: 2 * Math.pow(7.84, i),
+        baseCost: 11 * Math.pow(10.5, i),
+    };
+    data.generators.push(generator);
+    let g = data.generators[i];
+    data.cost.push(g.baseCost * Math.pow(1.07, g.amount));
+}
+
+let scrapPerSecond = 0;
+let buyAmount = 1;
+let lastUpdate = Date.now();
+
+const generatorsMenuContainerElement = document.getElementById("generators-container");
+const prestigeMenuContainerElement = document.getElementById("prestige-container");
+const settingsMenuContainerElement = document.getElementById("settings-container");
+
+let activeMenu = generatorsMenuContainerElement;
+prestigeMenuContainerElement.style.display = "none";
+settingsMenuContainerElement.style.display = "none";
+
+// UI
+//#region 
+
+const scrapTextElement = document.getElementById("scrap-text");
+const scrapPerSecondTextElement = document.getElementById("scrap-per-second-text");
+const generatorsMenuButtonElement = document.getElementById("generators-menu-button");
+
+function format(amount, numDigits = 2) {
+    let power = Math.floor(Math.log10(amount));
+    let mantissa = amount/Math.pow(10, power);
+    if (power < 3) return amount.toFixed(numDigits);
+    else return mantissa.toFixed(2) + "e" + power;
+}
+
+function openMenu(clickedMenu) {
+    activeMenu.style.display = "none";
+    activeMenu = clickedMenu;
+    if (activeMenu == generatorsMenuContainerElement) activeMenu.style.display = "flex";
+    else activeMenu.style.display = "block";
+}
+
+function updateScrapInfo() {
+    scrapTextElement.textContent = "You have " + format(data.scrap) + " Scrap";
+    scrapPerSecondTextElement.textContent = "You are making " + format(scrapPerSecond) + " Scrap/s";
+}
+
+function updateScrapPerSecond() {
+    scrapPerSecond = 0;
+    for (let i = 0; i < 8; i++) {
+        let g = data.generators[i];
+        scrapPerSecond += g.amount * g.sps;
+    }
+}
+
+function scrapProductionLoop(deltaTime) {
+    let scrapsToGain = scrapPerSecond * (1 + (data.transistors * data.transistorsBonus / 100)) * deltaTime;
+    data.scrap += scrapsToGain;
+    data.scrapsThisRun +=  scrapsToGain;
+    data.totalScrap +=  scrapsToGain;
+    transistorsGainedFromRestart = (150 * Math.sqrt(data.scrapsThisRun/(400000000000/9)));
+}
+
+
+
+function mainLoop() {
+    const now = Date.now();
+    const deltaTime = (now - lastUpdate) / 1000;
+    lastUpdate = now;
+    updateScrapInfo();
+    scrapProductionLoop(deltaTime);
+    updateGeneratorButtonColor();
+    updateTransistorInfo();
+}
+
+window.onload = function() {
+    loadData();
+    updateScrapPerSecond();
+    updateGeneratorInfo();
+}
+
+setInterval(mainLoop, 50);
+setInterval(saveData, 15000); // saves every 15s
+//#endregion
+
+// GENERATORS
+//#region
+
+const buyOneButtonElement = document.getElementById("buy-one-button");
+const buyTenButtonElement = document.getElementById("buy-ten-button");
+const buyHundredButtonElement = document.getElementById("buy-hundred-button");
+
+buyOneButtonElement.style.color = '#FBBF77';
+
+function updateGeneratorButtonColor() {
+    for (let i = 0; i < 8; i++) {
+        if (data.scrap < data.cost[i]) {
+            document.getElementById("gen" + (i + 1) + "-button").style.borderColor = 'Red';
+            document.getElementById("gen" + (i + 1) + "-button").style.cursor = "not-allowed";
+        } else {
+            document.getElementById("gen" + (i + 1) + "-button").style.borderColor = 'Green';
+            document.getElementById("gen" + (i + 1) + "-button").style.cursor = "default";
+        }
+    }
+}
+
+function updateGeneratorInfo() {
+    for (let i = 0; i < 8; i++) {
+        let g = data.generators[i];
+        document.getElementById("gen" + (i + 1) + "-amount").innerHTML = g.amount;
+        document.getElementById("gen" + (i + 1) + "-sps").innerHTML = format(g.sps);
+        document.getElementById("gen" + (i + 1) + "-cost").innerHTML = format(data.cost[i]);
+    }
+}
+
+function updateGeneratorCost() {
+    for (let i = 0; i < 8; i++) {
+        let g = data.generators[i];
+        data.cost[i] = g.baseCost * Math.pow(1.15, g.amount);
+    }
+}
+
+function buyGenerator(i) {
+    let g = data.generators[i - 1];
+    let c = data.cost[i - 1];
+    for (let i = 0; i < buyAmount; i++) {
+        if (data.scrap < c) return;
+        data.scrap -= c;
+        g.amount++;
+        updateScrapPerSecond();
+        updateGeneratorCost();
+        updateGeneratorInfo();
+    }
+}
+
+function changeBuyAmount(amount) {
+    buyAmount = amount;
+
+    switch(amount) {
+        case 1:
+            buyOneButtonElement.style.color = '#FBBF77';
+            buyTenButtonElement.style.color = 'White';
+            buyHundredButtonElement.style.color = 'White';
+            break;
+        case 10:
+            buyOneButtonElement.style.color = 'White';
+            buyTenButtonElement.style.color = '#FBBF77';
+            buyHundredButtonElement.style.color = 'White';
+            break;
+        case 100:
+            buyOneButtonElement.style.color = 'White';
+            buyTenButtonElement.style.color = 'White';
+            buyHundredButtonElement.style.color = '#FBBF77';
+            break;
+    }
+}
+// #endregion
+
+// PRESTIGE
+//#region 
+
+const totalTransistorsElement = document.getElementById("total-transistors");
+const gainedFromRestartElement = document.getElementById("gained-from-restart");
+const bonusFromTransistorsElement = document.getElementById("bonus-from-transistors");
+
+function updateTransistorInfo() {
+    totalTransistorsElement.innerHTML = format(data.transistors, 0);
+    gainedFromRestartElement.innerHTML = format(transistorsGainedFromRestart, 0);
+    bonusFromTransistorsElement.innerhtml = (data.transistorsBonus - 1) * 100;
+}
+
+function doPrestige() {
+    if (transistorsGainedFromRestart < 1) return;
+
+    data.scrap = 11;
+    data.generators = [];
+    data.cost = [];
+
+    for (let i = 0; i < 8; i++) {
+        let generator = {
+            amount: 0,
+            sps: 2 * Math.pow(7.84, i),
+            baseCost: 11 * Math.pow(10.5, i),
+        };
+        data.generators.push(generator);
+        let g = data.generators[i];
+        data.cost.push(g.baseCost * Math.pow(1.15, g.amount));
+    }
+
+    data.scrapsThisRun = 0;
+    data.transistors += transistorsGainedFromRestart;
+
+    updateScrapPerSecond();
+    updateGeneratorCost();
+    updateGeneratorInfo();
+}
+//#endregion
+
+// SETTINGS
+//#region 
+function saveData() {
+    window.localStorage.setItem('EnergyIdleSave', JSON.stringify(data));
+}
+
+function loadData() {
+    let savedGame = JSON.parse(localStorage.getItem('EnergyIdleSave'));
+    if (savedGame !== null) data = savedGame;
+}
+
+function resetData() {
+    data.scrap = 11;
+    data.totalScrap = 11;
+    data.scrapsThisRun = 0;
+    data.generators = [];
+    data.cost = [];
+    data.transistors = 0;
+    data.transistorsBonus = 1.02;
+
+    for (let i = 0; i < 8; i++) {
+        let generator = {
+            amount: 0,
+            sps: 2 * Math.pow(7.84, i),
+            baseCost: 11 * Math.pow(10.5, i),
+        };
+        data.generators.push(generator);
+        let g = data.generators[i];
+        data.cost.push(g.baseCost * Math.pow(1.15, g.amount));
+    }
+
+    updateScrapPerSecond();
+    updateGeneratorCost();
+    updateGeneratorInfo();
+}
+//#endregion
+
